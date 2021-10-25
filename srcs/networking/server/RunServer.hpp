65d816a0788a85b54d13859ef99deaa5e07a1276
vbuonvin <vbuonvin@e4r5p1.42roma.it>
2021-10-25 13:59:24 +0200

    up

--- srcs/networking/server/RunServer.hpp
+++ srcs/networking/server/RunServer.hpp
@@ -30,21 +30,18 @@ void RunServer::accepter(){
 	struct sockaddr_in remote = getServerSocket()->getRemote();
 	int remoteLen = sizeof(remote);
 	newSocket = accept(getServerSocket()->getSocket(), (struct sockaddr *)&remote, (socklen_t * )&remoteLen);
-	while(recv(newSocket, buff, 800000, 0)){
-		handler();
-		//responder();
-		bzero(buff, sizeof(buff));
-	}
-
+	recv(newSocket, buff, 800000, 0);
+	//responder();
 }
 
 void RunServer::handler(){
 	std::cout<< buff << std::endl;
+	bzero(buff, sizeof(buff));
 }
 
 void RunServer::responder(){
 
-	std::string message("CAP * LS :");
+	std::string message("PONG :0.0.0.0");
 	write(newSocket, message.c_str(), message.length());
 	//close(newSocket);
 }
@@ -53,9 +50,8 @@ void RunServer::launch(){
 	while(true){
 		std::cout << "------------WAITING-------------" << std::endl;
 		accepter();
-		//handler();
-		//responder();
+		handler();
+		responder();
 		std::cout << "------------DONE-----------" << std::endl;
-
 	}
 }
